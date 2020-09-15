a31e9de98765e3b20d70460c7245158fc2286679
Marlon Moro <marlon@ciclic.com.br>
2020-09-15 19:24:38 -0300

    Project created

---
+++ backend/src/main/java/br/com/devs/DevsPesquisa/DevsPesquisaApplication.java
@@ -0,0 +1,13 @@
+package br.com.devs.DevsPesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsPesquisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsPesquisaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/devs/DevsPesquisa/DevsPesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devs.DevsPesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsPesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
