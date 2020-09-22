049f1fb6bfb6c560d4c0cfc21caf04ad0d184a76
Marlon Moro <marlon@ciclic.com.br>
2020-09-21 21:13:55 -0300

    Rotas de navegacao

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './styles.css';
 import { ReactComponent as Arrowicon } from '../../assets/arrow.svg';
 import { ReactComponent as GamerImg } from '../../assets/gamer.svg';
+import { Link } from 'react-router-dom';
 
 
 const Home = () => (
@@ -9,12 +10,14 @@ const Home = () => (
         <div className="home-text">
             <h1 className="home-text-title">Quais jogos a galera gosta mais?</h1>
             <h3 className="home-text-subtitle">Clique no botão abaixo e saiba quais são os jogos que os gamers estão escolhendo!</h3>
-            <div className="home-actions">
-                <button className="home-btn">QUERO SABER QUAIS SÃO</button>
-                <div className="home-btn-icon">
-                    <Arrowicon />
+            <Link to="/records">
+                <div className="home-actions">
+                    <button className="home-btn">QUERO SABER QUAIS SÃO</button>
+                    <div className="home-btn-icon">
+                        <Arrowicon />
+                    </div>
                 </div>
-            </div>
+            </Link>
         </div>
         <GamerImg className="home-image"/>
     </div>
